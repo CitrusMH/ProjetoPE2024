3d359c3f0abb7a2439d26688dac91bb70f6e34db
Matheus Melo <klamatheus@hotmail.com>
2024-05-14 21:24:50 -0300

    Novo hero, nova tela de login e ajustes de msg no controller de user

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,27 +2,36 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const Usuarios = require('../models/Usuarios');
 
+exports.mostarCadastro = (req, res, next) => {
+    res.render ('cadastrousuario', { msg: '' });
+};
 exports.create = (req, res, next) => {
     const email = req.body.email;
     const senha = req.body.senha;
 
-    let salt = bcrypt.genSaltSync(10);
-    let senhaHash = bcrypt.hashSync(senha, salt);
+    if (email == undefined || email == '' || email == ' ' || email == null || email.length < 5 || email.indexOf('@') == -1 || email.indexOf('.') == -1 || email.indexOf(' ') != -1) {
+        res.render('cadastroUsuario', { msg: 'Email inválido!'});
+    }else if (senha == undefined || senha == '' || senha == ' ' || senha == null || senha.length < 8 || senha.indexOf(' ') != -1) {
+        res.render('cadastroUsuario', { msg: 'Senha inválida! A senha deve conter no mínimo 8 caracteres e não pode conter espaços!'});
+    }else{
+        let salt = bcrypt.genSaltSync(10);111
+        let senhaHash = bcrypt.hashSync(senha, salt);
 
-    Usuarios.findOne({ where: { email: email } }).then((usuario) => {
-        if (usuario == undefined) {
-            Usuarios.create({
-                email: email,
-                senha: senhaHash
-            }).then(() => {
-                res.status(201).send("Usuário criado com sucesso!");
-            }).catch((error) => {
-                res.status(400).send("Houve um erro na criação do usuário!");
-            });
-        } else {
-            res.status(400).send("Usuário já cadastrado!");
-        }
-    });   
+        Usuarios.findOne({ where: { email: email } }).then((usuario) => {
+            if (usuario == undefined) {
+                Usuarios.create({
+                    email: email,
+                    senha: senhaHash
+                }).then(() => {
+                    res.render('login', { msg: ''});
+                }).catch((error) => {
+                    res.render('cadastroUsuario', { msg: error});
+                });
+            } else {
+                res.render('cadastroUsuario', { msg: 'Usuário já existe' });
+            }
+        });
+    }   
 }
 
 exports.login = (req, res, next) => {
@@ -49,5 +58,5 @@ exports.login = (req, res, next) => {
 };
 
 exports.mostrarLogin = (req, res, next) => {
-    res.render('login');
+    res.render('login', { msg: '' });
 };
